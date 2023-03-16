d662cca742d43104bef8d130904770f04a553504
TheWayMaker007 <bokoefioterence@gmail.com>
2023-03-16 16:34:19 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * A program that prints a line with puts function not printf
+ * return(0);
+ */
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle\n");
+ return (0);
+}
