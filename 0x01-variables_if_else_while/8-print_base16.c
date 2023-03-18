138639a6930df65dc651169d7ac8ce703f07e092
TheWayMaker007 <bokoefioterence@gmail.com>
2023-03-17 18:20:56 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - program that prints the alphabet in lowercase, and then in uppercase
+ *Return: 0 (Successful)
+ */
+int main(void)
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	putcjar(d);
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+}
