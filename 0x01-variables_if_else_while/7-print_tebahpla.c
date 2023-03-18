4dfa054fe44dde33771f95baa41ec274d20d8fd2
TheWayMaker007 <bokoefioterence@gmail.com>
2023-03-17 18:12:07 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - program that prints the alphabet in lowercase, and then in uppercase
+ *Return: 0 (Successful)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low__)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+}
