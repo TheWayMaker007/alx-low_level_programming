1a8f312857abeb4c974dc2e444636a9dd80156d9
TheWayMaker007 <bokoefioterence@gmail.com>
2023-03-17 18:08:20 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - program that prints the alphabet in lowercase, and then in uppercase
+ *Return: 0 (Successful)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+}
