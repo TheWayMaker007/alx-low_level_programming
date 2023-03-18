ab77c802b7cfed8cbe16c1a7a100b340612ff0ae
TheWayMaker007 <bokoefioterence@gmail.com>
2023-03-17 18:02:56 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - program that prints the alphabet in lowercase, and then in uppercase
+ *Return: 0 (Successful)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
