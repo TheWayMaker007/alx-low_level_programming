ff1ef97c3024273c894f001b0e0fa1aef8a10213
TheWayMaker007 <bokoefioterence@gmail.com>
2023-03-23 17:32:29 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Check lowercase
+ * @c: The check value
+ * Return: 1 fpor upper or 0 for lower
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
