5ca9513f2d1fb3de6914591fe8d273b1f4b3b6a9
TheWayMaker007 <bokoefioterence@gmail.com>
2023-03-30 16:50:16 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - Write a function that encodes a string using rot13
+ * @s: pointer
+ * return: *s
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char data1[] =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char data2[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = data2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
