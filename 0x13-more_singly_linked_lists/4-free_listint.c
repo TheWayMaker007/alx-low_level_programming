f327240805d6f128812cf2706fd66956539a030b
Group Projects on 'printf <bokoefioterence@gmail.com>
2023-05-02 15:05:15 -0700

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_listint - a function that frees a listint_t list
+* @head: listint_t list to be freed
+* Returns: nothing
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *tempnode;
+
+	while (head)
+	{
+		tempnode = head->next;
+		free(head);
+		head = tempnode;
+	}
+}
